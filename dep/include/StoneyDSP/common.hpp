87c9bd2582dd29a230a53e7ad282fa632c3a393a
StoneyDSP <nathanjhood@googlemail.com>
2024-11-14 21:46:29 +0100

    edit 'dep/include/StoneyDSP/common.hpp' - testing fix of SSE2/SSE4.2 macros
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- dep/include/StoneyDSP/common.hpp
+++ dep/include/StoneyDSP/common.hpp
@@ -49,19 +49,30 @@
 //   #include <string_view> // Not supported by Rack!! :(
 // #endif
 
-#if defined (__SSE2__)
+// #if defined (__SSE2__)
+//  #if __has_include(<emmintrin.h>)
+//   #include <emmintrin.h>
+//  #else
+//   #define SIMDE_ENABLE_NATIVE_ALIASES
+//   #include <simde/x86/sse2.h> // The appropriate header file for emmintrin.h is simde/x86/sse2.h
+//  #endif
+// #elif defined(__aarch64__)
+//  #if __has_include(<sse2neon.h>)
+//   #include <sse2neon.h>
+//  #else
+//   #warning SSE2 support is not available. Code will not compile
+//  #endif
+// #endif
+
+#if defined(__SSE2__) || defined(__aarch64__)
  #if __has_include(<emmintrin.h>)
   #include <emmintrin.h>
  #else
   #define SIMDE_ENABLE_NATIVE_ALIASES
-  #include <simde/x86/sse2.h> // The appropriate header file for emmintrin.h is simde/x86/sse2.h
+  #include <simde/x86/sse2.h>
  #endif
-#elif defined(__aarch64__)
- #if __has_include(<sse2neon.h>)
-  #include <sse2neon.h>
- #else
-  #warning SSE2 support is not available. Code will not compile
- #endif
+#else
+  #warning SSE2/NEON support is not available. Code will not compile.
 #endif
 
 #if defined (__SSE4_2__) && __has_include(<nmmintrin.h>)
