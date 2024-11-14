8130db286f356dad3335bc7f7c85a35adf0e9671
StoneyDSP <nathanjhood@googlemail.com>
2024-11-14 20:34:12 +0100

    edit 'dep/include/StoneyDSP/common.hpp' - fixed typo on include simde line
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- dep/include/StoneyDSP/common.hpp
+++ dep/include/StoneyDSP/common.hpp
@@ -59,7 +59,7 @@
  #include <nmmintrin.h>
 #else
  #define SIMDE_ENABLE_NATIVE_ALIASES
- #include "<simde/x86/sse4.2.h>"
+ #include <simde/x86/sse4.2.h>
 #endif
 
 //==============================================================================
