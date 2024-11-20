cfe504762da59fc36c2a3698428b4c290e02498f
StoneyDSP <nathanjhood@googlemail.com>
2024-11-20 17:22:05 +0100

    edit 'dep/vcpkg/ports/stoneydsp/0.0.0/portfile.cmake' - cleanup
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- dep/vcpkg/ports/stoneydsp/0.0.0/portfile.cmake
+++ dep/vcpkg/ports/stoneydsp/0.0.0/portfile.cmake
@@ -1,16 +1,3 @@
-# vcpkg_from_github(
-#   OUT_SOURCE_PATH SOURCE_PATH
-#   REPO northwindtraders/beison
-#   REF a16bd37a342a20357ecfc67e8ce550b7a414bcb0
-#   SHA512 2bbefe9d59b9e0f2e7192570eb744ddbb500277be8d51df9be6e9a4449710d3ce6030a6d5a6418871c8050418311be5002d95585432d3b624ff59344740aa6b8
-#   HEAD_REF main
-# )
-
-# vcpkg_configure_cmake(
-#   SOURCE_PATH "${SOURCE_PATH}"
-#   PREFER_NINJA
-# )
-
 function(get_this_dir)
     set(_this_dir "${CMAKE_CURRENT_FUNCTION_LIST_DIR}" PARENT_SCOPE)
 endfunction()
@@ -30,9 +17,10 @@ vcpkg_cmake_config_fixup(
     CONFIG_PATH "lib/cmake/StoneyDSP"
 )
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
 file(
-  INSTALL "${SOURCE_PATH}/LICENSE"
-  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-  RENAME copyright)
+    INSTALL "${SOURCE_PATH}/LICENSE"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+    RENAME copyright
+)
