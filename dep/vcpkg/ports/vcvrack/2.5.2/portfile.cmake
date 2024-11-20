93ee30925f7c93631ba1202d69d641a35518ad91
StoneyDSP <nathanjhood@googlemail.com>
2024-11-20 03:17:31 +0100

    edit 'dep/vcpkg/ports/vcvrack/2.5.2/portfile.cmake' - try cygpath on RACK_DIR
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- dep/vcpkg/ports/vcvrack/2.5.2/portfile.cmake
+++ dep/vcpkg/ports/vcvrack/2.5.2/portfile.cmake
@@ -6,9 +6,51 @@
 #   HEAD_REF main
 # )
 
+function(_normalize_path var)
+    message(STATUS "normalizing path: ${var}")
+    set(path "${${var}}")
+    file(TO_CMAKE_PATH "${path}" path)
+    while(path MATCHES "//")
+        string(REPLACE "//" "/" path "${path}")
+    endwhile()
+    string(REGEX REPLACE "/+$" "" path "${path}")
+    set("${var}" "${path}" PARENT_SCOPE)
+    message(STATUS "normalized path: ${var}")
+endfunction()
+
 function(get_this_dir)
     set(_this_dir "${CMAKE_CURRENT_FUNCTION_LIST_DIR}" PARENT_SCOPE)
 endfunction()
+
+set(VCVRACK_RACK_LIB_FILE_EXTENSION)
+if(VCPKG_TARGET_IS_MINGW)
+    set(VCVRACK_RACK_LIB_FILE_EXTENSION ".dylib")
+elseif(VCPKG_TARGET_IS_LINUX)
+    set(VCVRACK_RACK_LIB_FILE_EXTENSION ".so")
+elseif(VCPKG_TARGET_IS_OSX)
+    set(VCVRACK_RACK_LIB_FILE_EXTENSION ".dll.a")
+else()
+    message(FATAL_ERROR "VCVRack: unsupported platform")
+endif()
+
+# vcpkg_download_distfile(ARCHIVE
+#     URLS "@URL@"
+#     FILENAME "@FILENAME@"
+#     SHA512 @SHA512@
+# )
+
+# vcpkg_extract_source_archive_ex(
+#     OUT_SOURCE_PATH SOURCE_PATH
+#     ARCHIVE "${ARCHIVE}"
+#     # (Optional) A friendly name to use instead of the filename of the archive (e.g.: a version number or tag).
+#     # REF 1.0.0
+#     # (Optional) Read the docs for how to generate patches at:
+#     # https://github.com/microsoft/vcpkg-docs/blob/main/vcpkg/examples/patching.md
+#     # PATCHES
+#     #   001_port_fixes.patch
+#     #   002_more_port_fixes.patch
+# )
+
 
 get_this_dir()
 
@@ -17,14 +59,26 @@ get_filename_component(__stoneyvcv_dir "${_this_dir}/../../../../../" ABSOLUTE)
 set(SOURCE_PATH "${__stoneyvcv_dir}/dep/VCVRack")
 
 vcpkg_backup_env_variables(
-    VARS _RACK_DIR
-    RACK_DIR
+    VARS RACK_DIR
 )
 
+if(VCPKG_TARGET_IS_MINGW)
+    # _normalize_path("${z_vcpkg_env_backup_RACK_DIR}")
+    message(STATUS "normalizing Rack SDK Path: ${z_vcpkg_env_backup_RACK_DIR}")
+    vcpkg_execute_in_download_mode(
+        COMMAND "cygpath -u ${z_vcpkg_env_backup_RACK_DIR}"
+        RESULT_VARIABLE _RACK_DIR #error_code
+        # WORKING_DIRECTORY "${arg_WORKING_DIRECTORY}"
+    )
+    if("${_RACK_DIR}" NOT STREQUAL "")
+        message(FATAL_ERROR "${error_variable}")
+    endif()
+    set(z_vcpkg_env_backup_RACK_DIR "${_RACK_DIR}" CACHE)
+endif()
 
 vcpkg_cmake_configure(
-  SOURCE_PATH "${SOURCE_PATH}"
-  OPTIONS -DRACK_DIR="${_RACK_DIR}"
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS -DRACK_DIR="${z_vcpkg_env_backup_RACK_DIR}"
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(
@@ -33,8 +87,14 @@ vcpkg_cmake_config_fixup(
 )
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib" "${CURRENT_PACKAGES_DIR}/debug/lib")
 
 file(
-  INSTALL "${SOURCE_PATH}/LICENSE"
-  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-  RENAME copyright)
+    INSTALL "${SOURCE_PATH}/LICENSE"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+    RENAME copyright
+)
+
+vcpkg_restore_env_variables(
+    VARS RACK_DIR
+)
