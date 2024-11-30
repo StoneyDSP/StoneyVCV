4f6f64f76e446b1f902a0b5a115180389a7fd584
StoneyDSP <nathanjhood@googlemail.com>
2024-11-30 15:05:58 +0100

    edit 'test/HP1.cpp' - add tests for module instance ptr and header include defs
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- test/HP1.cpp
+++ test/HP1.cpp
@@ -4,6 +4,14 @@
 
 // Tests go here...
 
-TEST_CASE("placeholder", "[HP1]") {
-    REQUIRE(true == true);
+TEST_CASE("HP1", "[HP1]") {
+
+    SECTION( "files" ) {
+        REQUIRE(STONEYVCV_HP1_HPP_INCLUDED == 1);
+    }
+
+    SECTION( "instance" ) {
+        REQUIRE(::StoneyDSP::VCVRack::modelHP1 != nullptr);
+    }
+
 }
