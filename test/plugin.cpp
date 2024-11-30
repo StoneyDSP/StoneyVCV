0372bc7fbc6e1ec8f82844dfca778f2848a8a3a9
StoneyDSP <nathanjhood@googlemail.com>
2024-11-30 13:44:13 +0100

    edit 'test/plugin.cpp' - cleanup
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- test/plugin.cpp
+++ test/plugin.cpp
@@ -1,26 +1,9 @@
-// #include "plugin.hpp"
-
-// #ifndef PLUGIN_HPP_INCLUDED
-// #error "Couldn't find 'plugin.hpp'?"
-// #endif
-
-// #undef WARN
-
-#include <rack.hpp>
-
-// #define CATCH_CONFIG_MAIN
-
-#if __has_include(<catch2/catch_test_macros.hpp>)
 #include <catch2/catch_test_macros.hpp>
-#define STONEYDSP_HAS_CATCH2 1
-#else
-#warning "Can't find Catch2 headers for unit tests!"
-#endif
 
-#if STONEYDSP_HAS_CATCH2
+#include "plugin.hpp"
 
 // Tests go here...
 
-TEST_CASE("placeholder", "[plugin]") { REQUIRE(true == true); }
-
-#endif // STONEYDSP_HAS_CATCH2
+TEST_CASE("placeholder", "[plugin]") {
+    REQUIRE(true == true);
+}
