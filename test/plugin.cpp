fadf6580c66bd3c6ae5c2323a2d814a7cc7e8c4a
StoneyDSP <nathanjhood@googlemail.com>
2024-11-30 15:00:25 +0100

    edit 'test/plugin.cpp' - add some tests for pointer instances and header includes
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- test/plugin.cpp
+++ test/plugin.cpp
@@ -4,6 +4,27 @@
 
 // Tests go here...
 
-TEST_CASE("placeholder", "[plugin]") {
-    REQUIRE(true == true);
+TEST_CASE("plugin", "[plugin]") {
+
+    SECTION( "files" ) {
+        REQUIRE(STONEYVCV_PLUGIN_HPP_INCLUDED == 1);
+    }
+
+    SECTION( "pluginInstance" ) {
+        REQUIRE(::StoneyDSP::VCVRack::pluginInstance != nullptr);
+    }
+
+    SECTION("modules") {
+#ifdef STONEYVCV_EXPERIMENTAL
+    // EXPERIMENTAL MODULES HERE...
+#endif
+
+#if (STONEYVCV_VERSION_MAJOR >= 0) && (STONEYVCV_VERSION_MINOR >= 0) && (STONEYVCV_VERSION_PATCH >= 1)
+        SECTION( "HP1" ) {
+            REQUIRE(::StoneyDSP::VCVRack::modelHP1 != nullptr);
+        }
+#elif (STONEYVCV_VERSION_MAJOR) >= 0 && (STONEYVCV_VERSION_MINOR >= 0) && (STONEYVCV_VERSION_PATCH >= 0)
+    #warning "No modules found..."
+#endif
+    }
 }
