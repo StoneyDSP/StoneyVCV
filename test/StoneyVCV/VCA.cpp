7d0847a67aad39b35b4f8d856c6acbe8b17a23f9
StoneyDSP <nathanjhood@googlemail.com>
2024-12-04 22:01:21 +0100

    add '*/StoneyVCV/VCA.{cpp,hpp}'

---
+++ test/StoneyVCV/VCA.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file VCA.cpp
+ * @author Nathan J. Hood <nathanjhood@googlemail.com>
+ * @brief
+ * @version 0.0.0
+ * @date 2024-11-11
+ *
+ * @copyright Copyright (c) 2024
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * therights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/orsell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ ******************************************************************************/
+
+#if (STONEYVCV_BUILD_VCA == 1) && (STONEYVCV_BUILD_TESTS == 1)
+
+#include <catch2/catch_test_macros.hpp>
+// for floating point comparisons
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+
+#include "StoneyVCV/VCA.hpp"
+
+// Spec goes here...
+
+#endif
