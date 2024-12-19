6ef7e93b8ce27cdb92ad7bf3b3bf12d06400787a
StoneyDSP <nathanjhood@googlemail.com>
2024-12-19 21:39:36 +0100

    edit '{include,src,test}/StoneyVCV/[HP1,HP2,HP4].{cpp,hpp}' - better include pattern for more optimal object libs

--- test/StoneyVCV/HP4.cpp
+++ test/StoneyVCV/HP4.cpp
@@ -35,12 +35,12 @@
 
 //==============================================================================
 
-#include <catch2/catch_test_macros.hpp>
-#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include <StoneyVCV/HP4.hpp>
 
 //==============================================================================
 
-#include <StoneyVCV/HP4.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 
 //==============================================================================
 
