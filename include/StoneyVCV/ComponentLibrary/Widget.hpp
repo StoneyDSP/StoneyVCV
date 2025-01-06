42d4fac1b9abbccccccc0852370349d63d3d306b
StoneyDSP <nathanjhood@googlemail.com>
2025-01-07 00:14:56 +0100

    add '{include,src}/StoneyVCV/ComponentLibrary/Widget.{cpp,hpp}'
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ include/StoneyVCV/ComponentLibrary/Widget.hpp
@@ -0,0 +1,165 @@
+/*******************************************************************************
+ * @file include/StoneyVCV/ComponentLibrary/Widget.hpp
+ * @author Nathan J. Hood <nathanjhood@googlemail.com>
+ * @brief @PROJECT_DESCRIPTION@
+ * @version @plugin_VERSION@
+ *
+ * @copyright MIT License
+ *
+ * Copyright (c) 2024 Nathan J. Hood <nathanjhood@googlemail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ ******************************************************************************/
+
+#pragma once
+
+#define STONEYVCV_COMPONENTLIBRARY_PANELWIDGET_HPP_INCLUDED 1
+
+#if defined (STONEYVCV_BUILD_COMPONENTLIBRARY)
+
+//==============================================================================
+
+#include <StoneyVCV/ComponentLibrary.hpp>
+
+//==============================================================================
+
+#include <rack.hpp>
+#include <StoneyDSP/Core.hpp>
+
+//==============================================================================
+
+namespace StoneyDSP
+{
+/** @addtogroup StoneyDSP
+ *  @{
+ */
+
+//==============================================================================
+
+namespace StoneyVCV
+{
+/** @addtogroup StoneyVCV
+ *  @{
+ */
+
+//==============================================================================
+
+namespace ComponentLibrary
+{
+/** @addtogroup ComponentLibrary
+ *  @{
+ */
+
+//==============================================================================
+
+/**
+ * @brief The `ThemedWidget` struct.
+ *
+ */
+struct ThemedWidget : virtual ::rack::widget::Widget
+{
+
+    //==========================================================================
+
+public:
+
+    using DrawArgs = ::rack::widget::Widget::DrawArgs;
+
+    //==========================================================================
+
+    /**
+     * @brief Construct a new `ThemedWidget` object.
+     *
+     */
+    ThemedWidget();
+
+    /**
+     * @brief Destroys the `ThemedWidget` object.
+     *
+     */
+    virtual ~ThemedWidget();
+
+    //==========================================================================
+
+    /**
+     * @brief Advances the module by one frame.
+     *
+     */
+    virtual void step() override;
+
+    /**
+     * @brief Draws a themed background color to the widget's NanoVG context.
+     *
+     * @param args
+     */
+    void drawThemedBg(const ::StoneyDSP::StoneyVCV::ComponentLibrary::ThemedWidget::DrawArgs &args);
+
+    /**
+     * @brief Draws the widget to the NanoVG context.
+     * Calls the superclass's draw(args) to recurse to children.
+     *
+     * @param args
+     */
+    virtual void draw(const ::StoneyDSP::StoneyVCV::ComponentLibrary::ThemedWidget::DrawArgs &args) override;
+
+    //==========================================================================
+
+private:
+
+    //==========================================================================
+
+    /**
+     * @brief
+     *
+     */
+    ::rack::FramebufferWidget *fb;
+
+    /**
+     * @brief
+     *
+     */
+    ::rack::app::PanelBorder *panelBorder;
+
+    //==========================================================================
+
+    STONEYDSP_DECLARE_NON_COPYABLE(ThemedWidget)
+    STONEYDSP_DECLARE_NON_MOVEABLE(ThemedWidget)
+};
+
+//==============================================================================
+
+  /// @} group ComponentLibrary
+} // namespace ComponentLibrary
+
+//==============================================================================
+
+  /// @} group StoneyVCV
+} // namespace StoneyVCV
+
+//==============================================================================
+
+  /// @} group StoneyDSP
+} // namespace StoneyDSP
+
+//==============================================================================
+
+#endif // STONEYVCV_BUILD_COMPONENTLIBRARY
+
+//==============================================================================
