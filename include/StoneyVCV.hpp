cb757ab96694d83d9ca16eb2d72e017ed17fd808
StoneyDSP <nathanjhood@googlemail.com>
2025-01-07 00:07:05 +0100

    edit 'include/StoneyVCV.hpp' - move globals and helpers here
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- include/StoneyVCV.hpp
+++ include/StoneyVCV.hpp
@@ -39,6 +39,15 @@
 
 //==============================================================================
 
+#ifdef STONEYDSP_DEBUG
+#include <iostream>
+#define DBG(msg, ...) do { ::std::cerr << std::string(msg, ##__VA_ARGS__) << std::endl; } while (0)
+#else
+#define DBG(msg, ...) ::StoneyDSP::ignoreUnused(msg, ##__VA_ARGS__)
+#endif
+
+//==============================================================================
+
 /**
  * @brief The `StoneyDSP` namespace.
  * @author Nathan J. Hood (nathanjhood@googlemail.com)
@@ -68,6 +77,89 @@ namespace StoneyVCV
 
 //==============================================================================
 
+namespace Tools {
+/** @addtogroup Tools
+ *  @{
+ */
+
+//==============================================================================
+
+const extern ::StoneyDSP::float_t vMin;
+const extern ::StoneyDSP::float_t vMax;
+const extern ::StoneyDSP::float_t vNominal;
+const extern ::StoneyDSP::float_t vBias;
+const extern ::StoneyDSP::float_t vGround;
+const extern ::StoneyDSP::float_t vFloor;
+
+//==============================================================================
+
+  /// @} group Tools
+} // namespace Tools
+
+//==============================================================================
+
+// Declare an abstract base class with a pure virtual destructor.
+// It's the simplest possible abstract class.
+template <typename T>
+struct Engine
+{
+
+    //==========================================================================
+
+public:
+
+    //==========================================================================
+
+    Engine()
+    {
+        DBG("Creating StoneyDSP::StoneyVCV::Engine");
+    };
+
+    virtual ~Engine() noexcept = 0;                                             // pure virtual
+
+    //==========================================================================
+
+    virtual void processSample(T* sample) = 0;                                  // pure virtual
+
+    //==========================================================================
+
+private:
+
+    //==========================================================================
+
+    STONEYDSP_DECLARE_NON_COPYABLE(Engine)
+    STONEYDSP_DECLARE_NON_MOVEABLE(Engine)
+};
+
+template<class T>
+::StoneyDSP::StoneyVCV::Engine<T>::~Engine() noexcept
+{
+    DBG("Destroying StoneyDSP::StoneyVCV::Engine");
+}
+
+template struct ::StoneyDSP::StoneyVCV::Engine<::StoneyDSP::float_t>;
+template struct ::StoneyDSP::StoneyVCV::Engine<::StoneyDSP::double_t>;
+
+//==============================================================================
+
+template <class TWidget = ::rack::widget::Widget>
+TWidget *createWidgetSized(::rack::math::Vec pos, ::rack::math::Vec size)
+{
+    TWidget* o = ::rack::createWidget<TWidget>(pos);
+	o->box.size = size;
+	return o;
+}
+
+template <class TWidget = ::rack::widget::Widget>
+TWidget* createWidgetCenteredSized(::rack::math::Vec pos, ::rack::math::Vec size)
+{
+    TWidget* o = ::rack::createWidgetCentered<TWidget>(pos);
+	o->box.size = size;
+	return o;
+}
+
+//==============================================================================
+
   /// @} group StoneyVCV
 } // namespace StoneyVCV
 
@@ -75,5 +167,15 @@ namespace StoneyVCV
 
   /// @} group StoneyDSP
 } // namespace StoneyDSP
+
+//==============================================================================
+
+// #include <StoneyVCV/version.hpp>
+// #include <StoneyVCV/ComponentLibrary.hpp>
+// #include <StoneyVCV/plugin.hpp>
+// #include <StoneyVCV/HP1.hpp>
+// #include <StoneyVCV/HP2.hpp>
+// #include <StoneyVCV/HP4.hpp>
+// #include <StoneyVCV/VCA.hpp>
 
 //==============================================================================
